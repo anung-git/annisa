d75b78a03e3431ff48b8fae84f5628e98f1644e3
anung <anung93@ymail.com>
2021-07-10 17:23:58 +0700

    android mqtt

--- arduino/gas-sensor/src/AndroidMqtt.cpp
+++ arduino/gas-sensor/src/AndroidMqtt.cpp
@@ -1,8 +1,75 @@
-
-
 #include "AndroidMqtt.h"
+void AndroidMqtt::reconnect()
+{
+    // Loop until we're reconnected
+    while (!client->connected())
+    {
+        // Create a random client ID
+        String clientId = "ESP8266Client-";
+        clientId += String(random(0xffff), HEX);
+        if (client->connect(clientId.c_str()))
+        {
+            client->subscribe(this->topic);
+        }
+        else
+        {
+            // Wait 5 seconds before retrying
+            delay(5000);
+        }
+    }
+}
+void AndroidMqtt::init()
+{
+    client = new PubSubClient(espClient);
+}
+void AndroidMqtt::loop()
+{
+    if (!client->connected())
+    {
+        reconnect();
+    }
+    client->loop();
+}
+
+void AndroidMqtt::callback(char *topic, byte *payload, unsigned int length)
+{
+    Serial.print("Message arrived [");
+    Serial.print(topic);
+    Serial.print("] ");
+    for (unsigned int i = 0; i < length; i++)
+    {
+        Serial.print((char)payload[i]);
+    }
+    Serial.println();
+
+    // Switch on the LED if an 1 was received as first character
+    if ((char)payload[0] == '1')
+    {
+        digitalWrite(BUILTIN_LED, LOW); // Turn the LED on (Note that LOW is the voltage level
+                                        // but actually the LED is on; this is because
+                                        // it is active low on the ESP-01)
+    }
+    else
+    {
+        digitalWrite(BUILTIN_LED, HIGH); // Turn the LED off by making the voltage HIGH
+    }
+}
+
+void AndroidMqtt::publish(const char *topic, const char *msg)
+{
+    Serial.print("android Publish ");
+    Serial.println(msg);
+    client->publish(topic, msg);
+}
+void AndroidMqtt::connect(const char *server, const char *topic)
+{
+    this->topic = topic;
+    client->setServer(server, 1883);
+    client->setCallback(this->callback);
+}
 AndroidMqtt::AndroidMqtt(/* args */)
 {
+    client = new PubSubClient(espClient);
 }
 
 AndroidMqtt::~AndroidMqtt()
--- arduino/gas-sensor/src/AndroidMqtt.h
+++ arduino/gas-sensor/src/AndroidMqtt.h
@@ -3,13 +3,25 @@
 
 #include <Arduino.h>
 
+#include <PubSubClient.h>
+
+#include <ESP8266WiFi.h>
 class AndroidMqtt
 {
 private:
     /* data */
-public:
-    AndroidMqtt(/* args */);
+    WiFiClient espClient;
+    PubSubClient *client;
+    const char *topic;
+    static void callback(char *topic, byte *payload, unsigned int length);
 
+public:
+    AndroidMqtt();
+    void init();
+    void loop();
+    void reconnect();
+    void publish(const char *topic, const char *msg);
+    void connect(const char *server,const char *topic);
     void sendMessage(String msg);
     void sendGasValue(float data);
     void fanOn();
